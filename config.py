f585af9a15fe482f14a6daed114749a402cd8e6b
Taha-Khurram <tahakhurramofficial@gmail.com>
2026-04-19 22:10:28 +0500

    Move Firebase config to environment variables

--- config.py
+++ config.py
@@ -8,7 +8,11 @@ class Config:
     FIREBASE_SERVICE_ACCOUNT = os.getenv('FIREBASE_SERVICE_ACCOUNT')
     GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
-    # Reconstruct the JS object for the frontend
+    # RapidAPI for SEO Keyword Research (free tier: 500 req/month)
+    # Get your key at: https://rapidapi.com
+    RAPIDAPI_KEY = os.getenv('RAPIDAPI_KEY')
+
+    # Firebase Web SDK Configuration (from environment variables)
     FIREBASE_CONFIG = {
         "apiKey": os.getenv("FB_API_KEY"),
         "authDomain": os.getenv("FB_AUTH_DOMAIN"),
@@ -18,14 +22,3 @@ class Config:
         "appId": os.getenv("FB_APP_ID"),
         "measurementId": os.getenv("FB_MEASUREMENT_ID")
     }
-    
-# For Firebase JS SDK v7.20.0 and later, measurementId is optional
-firebaseConfig = {
-  "apiKey": "AIzaSyDLj13Gxr9pZCT4drZxvf4vpWR7pD5LEtA",
-  "authDomain": "scriptly-80803.firebaseapp.com",
-  "projectId": "scriptly-80803",
-  "storageBucket": "scriptly-80803.firebasestorage.app",
-  "messagingSenderId": "1082423526791",
-  "appId": "1:1082423526791:web:ea1ea304119e110a31bf22",
-  "measurementId": "G-D4R1CPD5HF"
-};
